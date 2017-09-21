ff532da38ee92af25c46d92b047cd16bd35d3c6f
Legiotit <zum.q@mail.ru>
2017-09-21 23:57:40 +0500

    Move

--- Assets/Move.cs
+++ Assets/Move.cs
@@ -1,16 +1,37 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Move : MonoBehaviour {
+    public float speed,dist,gravity;
+    private int s;
+    private Vector3 newPosition;
+    public GameObject UI;
+    // Use this for initialization
+    void Start () {
+        newPosition = transform.position;
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    }
+    
+    // Update is called once per frame
+    void FixedUpdate() {
+
+       if (s< Input.touchCount) {
+            Vector3 touchPosition = new Vector3(Input.GetTouch(0).position.x, Input.GetTouch(0).position.y, 0);
+            newPosition = Camera.main.ScreenToWorldPoint(touchPosition);
+            newPosition = new Vector3(newPosition.x, newPosition.y,0);
+        }
+
+
+        if (Vector3.Distance(transform.position, newPosition) > dist) { transform.Translate(Vector3.Normalize((newPosition - transform.position)) * speed); }
+        else if (Vector3.Distance(transform.position, newPosition) > 0.001f) { transform.Translate(newPosition - transform.position); }
+
+        UI.GetComponent<Text>().text=" "+transform.position.x+ " " + transform.position.y+" " + transform.position.z+" "+s;
+
+        s = Input.touchCount;
+        newPosition=new Vector3(newPosition.x - (1* Statsgame.speed * Time.deltaTime),newPosition.y,0);
+
+
+    }
 }
---
+++ Assets/Moveroom.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Moveroom : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+    private void FixedUpdate()
+    {
+        if (transform.position.x < -3.3f) { transform.position = Vector3.right * 3.3f; }
+        transform.Translate(Vector3.left * Statsgame.speed * Time.deltaTime);
+    }
+    
+}
