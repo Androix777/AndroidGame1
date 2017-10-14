04187a1af1378d37077d014095eaa848340df3c0
Unknown <androix77@gmail.com>
2017-10-14 18:31:46 +0500

    Почти стабильная версия

--- Assets/Scripts/Generationroom.cs
+++ Assets/Scripts/Generationroom.cs
@@ -5,24 +5,25 @@ using UnityEngine.UI;
 
 public class Generationroom : MonoBehaviour {
     public string[] rooms;
-    float i;
+    public float speedroom;
     public float speedup;
+
 	// Use this for initialization
 	void Start () {
         Endroom();
         // i = 1;
-        i = 1;
+        
 	}
 
     private void FixedUpdate()
     {
-        i = i + speedup;
-        Statsgame.Setscore(Statsgame.Getscore()+i/10);
-        Statsgame.Setspeed(Mathf.Sqrt(i));
+		speedroom = speedroom + speedup;
+		Statsgame.Setscore(Statsgame.Getscore()+speedroom/10);
+		Statsgame.Setspeed(Mathf.Sqrt(speedroom));
     }
     
     public void Endroom() {
-       GameObject g= Instantiate(Resources.Load(rooms[Random.RandomRange(0,rooms.Length)]), new Vector3(100, 0, 0), transform.rotation) as GameObject;
+        if (rooms.Length>0) { GameObject g = Instantiate(Resources.Load(rooms[Random.RandomRange(0, rooms.Length)]), new Vector3(100, 0, 0), transform.rotation) as GameObject; }
     }
 
 }
